dbf9eadd0ba7f306891677cd847d91a3fa4a7b27
dylan-yoon <d.dylany21@gmail.com>
2022-09-27 11:21:55 +0900

    first commit 냠냠프로젝트 프로젝트 생성

---
+++ nYam_nYam/nYam_nYam/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  nYam_nYam
+//
+//  Created by Dylan_Y on 2022/09/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
